844878adad774234b22feca1609860ab9085534c
Ann-Marie <anniedillier@gmail.com>
2020-08-20 21:46:03 -0700

    calendar pick

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,16 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(taskDate);
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(taskText);
+
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+
+  var taskP = $("<p>").addClass("m-1").text(taskText);
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -110,7 +110,10 @@ $("#trash").droppable({
   out: function(event, ui) {
     console.log("out");
   }
+});
 
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // modal was triggered
@@ -143,19 +146,45 @@ $("#task-form-modal .btn-primary").click(function() {
       date: taskDate
     });
 
+    saveTasks();
+  }
+});
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+};
+
+//task test was clicked
 $(".list-group").on("click", "p", function() {
+  // get current text of p element
   var text = $(this)
     .text()
     .trim();
-   
+
+   // replace p element with a new textarea
   var textInput = $("<textarea>")
     .addClass("form-control")
     .val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
 
+    // auto focus new element
+  textInput.trigger("focus");
 });
 
+// editable field was un-focused
 $(".list-group").on("blur", "textarea", function() {
 // get the textarea's current value/text
 var text = $(this)
@@ -173,6 +202,7 @@ var index = $(this)
   .closest(".list-group-item")
   .index();
 
+// update task in array and re-save to localstorage
 tasks[status][index].text = text;
 saveTasks();
 
@@ -190,24 +220,32 @@ $(this).replaceWith(taskP);
 $(".list-group").on("click", "span", function() {
   // get current text
   var date = $(this)
-    .text()
-    .trim();
+  .text()
+  .trim();
 
   // create new input element
   var dateInput = $("<input>")
-    .attr("type", "text")
-    .addClass("form-control")
-    .val(date);
+  .attr("type", "text")
+  .addClass("form-control")
+  .val(date);
 
-  // swap out elements
   $(this).replaceWith(dateInput);
 
-  // automatically focus on new element
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the `dateInput`
+      $(this).trigger("change");
+    }
+  });
+
+  // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -232,13 +270,12 @@ $(".list-group").on("blur", "input[type='text']", function() {
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(date);
+    // replace input with span element
+    $(this).replaceWith(taskSpan);
 
-  // replace input with span element
-  $(this).replaceWith(taskSpan);
-});
-
-    saveTasks();
-  }
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
+  
 });
 
 // remove all tasks
@@ -248,7 +285,8 @@ $("#remove-tasks").on("click", function() {
     $("#list-" + key).empty();
   }
   saveTasks();
-});
+}); 
+
 
 // load tasks for the first time
 loadTasks();
